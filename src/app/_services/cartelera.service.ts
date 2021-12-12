c6b10b1ea00c2952e96d41c3241b6c6fe43019c4
Barone, Mauro Cesar <mauro_barone86@hotmail.com>
2021-12-12 20:47:15 -0300

    Primer modelo estable

---
+++ src/app/_services/cartelera.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from 'src/environments/environment';
+
+
+import { Cartelera } from '../_models/Cartelera';
+
+@Injectable({ providedIn: 'root' })
+export class CarteleraService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<Cartelera[]>(`${env.url}/cartelera`);
+    }
+}
